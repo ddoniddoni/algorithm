8b59a2ad45f326946706a2c13cb70fbde3a3d3c4
ddoniddoni <psdkei@naver.com>
2020-04-21 04:41:23 +0900

    learn Algorithm

---
+++ bigO.js
@@ -0,0 +1,7 @@
+const log = (array) => {
+  console.log(array[0]);
+  console.log(array[1]);
+};
+
+log([1, 2, 3]);
+log([1, 2, 3, 4, 5, 6]);
---
+++ harmlessRansom.js
@@ -0,0 +1,21 @@
+const harmlessRansomNote = (noteText, magazineText) => {
+  const noteArr = noteText.split("");
+  const magazineArr = magazineText.split("");
+  const magazineObj = {};
+
+  magazineArr.forEach((word) => {
+    if (!magazineObj[word]) magazineObj[word] = 0;
+    magazineObj[word]++;
+  });
+
+  const noteIsPossible = true;
+  noteArr.forEach((word) => {
+    if (magazineObj[word]) {
+      magazineObj[word]--;
+      if (magazineObj[word] < 0) noteIsPossible = false;
+    } else noteIsPossible = false;
+  });
+  return noteIsPossible;
+};
+
+harmlessRansomNote("", "");
