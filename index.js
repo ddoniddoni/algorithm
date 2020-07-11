ef373885960c17a2d88f1b6f2633c0ea0fe7ba01
ddoniddoni <psdkei@naver.com>
2020-07-11 23:09:33 +0900

    Update insertionSort

--- index.js
+++ index.js
@@ -1,45 +1,15 @@
-//ES2015 string에 새로운 메서드
-
-// let str = "hello world! ^^ ~~~~";
-// let matchStr = "hello";
-
-// console.log(str.startsWith(matchStr));
-// console.log(str.endsWith(matchStr));
-// console.log("include test", str.includes("^^"));
-
-//for of
-
-// 문자열 or 배열 등을 순회해서 출력할 수 있음
-// for (let value of str) {
-//   console.log(value);
-// }
-
-//spread operatior, 펼침 연산자
-
-// const sum = (a, b, c) => {
-//   return a + b + c;
-// };
-
-// const list = [100, 200, 300];
-// console.log(sum(...list));
-
-// ES2015 from Method
-
-// function addMark() {
-//   let newArray = Array.from(arguments);
-//   let newData = newArray.map((value) => {
-//     return value + "!";
-//   });
-//   console.log(newData);
-// }
-// addMark(1, 2, 3, 4, 5, 6, 7);
-
-const name = "ddoni";
-const age = 29;
-
-const obj = {
-  name,
-  age,
+const insertionSort = (array) => {
+  let i = 1,
+    j,
+    temp;
+  for (i; i < array.length; i++) {
+    temp = array[i]; // 새로운 숫자 선택
+    for (j = i - 1; j >= 0 && temp < array[j]; j--) {
+      array[j + 1] = array[j];
+    }
+    array[j + 1] = temp;
+  }
+  console.log(array);
 };
 
-console.log(obj);
+insertionSort([10, 25, 11, 2, 6, 76]);
