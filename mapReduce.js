f84c5655a7c53c38828fd9d06e2210cc0a73f127
ddoniddoni <psdkei@naver.com>
2020-07-11 22:39:00 +0900

    Add mapReduce

--- mapReduce.js
+++ mapReduce.js
@@ -1,6 +1,8 @@
-const oneTwoThree = [1, 2, 3];
+const oneTwoThree = [1, 2, 3, 4, 5, 6, 7];
 
-let result = oneTwoThree.reduce((acc, cur, i) => {
-  console.log(acc, cur, i);
-  return acc + cur;
-}, 0);
+let result = oneTwoThree.reduce((acc, cur) => {
+  acc.push(cur % 2 ? "홀수" : "짝수");
+  return acc;
+}, []);
+
+console.log(result);
