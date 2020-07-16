2eadfbb1991d73cbe25063510f31ecedb48c6cb2
ddoniddoni <psdkei@naver.com>
2020-07-16 18:00:44 +0900

    Update quickSort

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
-const selectionSort = (array) => {
-  let length = array.length;
-  let minIndex, temp, i, j;
-  for (i = 0; i < length - 1; i++) {
-    minIndex = i;
-    console.log(`${i}번째 숫자는`, array[minIndex]);
-    for (j = i + 1; j < length; j++) {
-      if (array[j] < array[minIndex]) {
-        minIndex = j;
-        console.log("j는", minIndex);
-      }
+const partition = (array, left, right, pivotIndex) => {
+  let temp;
+  let pivot = array[pivotIndex];
+  while (left <= right) {
+    while (array[left] < pivot) left++;
+    while (array[right] < pivot) right--;
+    if (left <= right) {
+      temp = array[left];
+      array[left] = array[right];
+      array[right] = temp;
+      left++;
+      right--;
     }
-    temp = array[minIndex];
-    array[minIndex] = array[i];
-    array[i] = temp;
-    console.log(array);
   }
-  // console.log(array);
+  temp = array[left];
+  array[left] = array[pivotIndex];
+  array[pivotIndex] = temp;
+  return left;
 };
 
-selectionSort([5, 4, 1, 7, 2, 6, 8, 3]);
+const quickSort = (array, left, right) => {
+  if (!left) left = 0;
+  if (!right) right = array.length - 1;
+  let pivotIndex = right;
+  pivotIndex = partition(array, left, right - 1, pivotIndex);
+  if (left < pivotIndex - 1) quickSort(array, left, pivotIndex - 1);
+  if (pivotIndex + 1 < right) quickSort(array, pivotIndex + 1, right);
+  console.log(array);
+};
+
+quickSort([4, 1, 7, 6, 3, 8, 2, 5]);
