680dcd0d82aafe32918de232f06eeeccc7181255
ddoniddoni <psdkei@naver.com>
2020-07-16 16:45:49 +0900

    Update bubbleSort

---
+++ js/bubbleSort.js
@@ -0,0 +1,16 @@
+const bubbleSort = (array) => {
+  let length = array.length;
+  let i, j, temp;
+  for (i = 0; i < length - 1; i++) {
+    for (j = 0; j < length - 1 - i; j++) {
+      if (array[j] > array[j + 1]) {
+        temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+  return console.log(array);
+};
+
+bubbleSort([5, 1, 7, 4, 6, 3, 2, 8]);
