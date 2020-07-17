492dab92ca2f230913eb53274793ba1b8326ba37
ddoniddoni <psdkei@naver.com>
2020-07-17 17:31:39 +0900

    Update css with selectionSort

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const num__input = document.querySelector(".num__input");
-const num__btn = document.querySelector(".num__btn");
+const selection__btn = document.querySelector(".selection__btn");
 let num__view = document.querySelector(".num__view");
 let numArray = [];
 
-const onSubmit = () => {
-  numArray = num__input.value;
-  console.log(numArray);
+// const onClick = () => {
+//   numArray = num__input.value.split(" ");
+//   for (let i = 0; i < numArray.length; i++) {
+//     console.log(numArray[i]);
+//   }
+// };
+
+const selectionSort = () => {
+  numArray = num__input.value.split(" ");
+  let length = numArray.length;
+  let minIndex, temp, i, j;
+  for (i = 0; i < length - 1; i++) {
+    minIndex = i;
+    console.log(`${i}번째 숫자는`, numArray[minIndex]);
+    for (j = i + 1; j < length; j++) {
+      if (numArray[j] < numArray[minIndex]) {
+        minIndex = j;
+        console.log("j는", minIndex);
+      }
+    }
+    temp = numArray[minIndex];
+    numArray[minIndex] = numArray[i];
+    numArray[i] = temp;
+    console.log(numArray);
+  }
 };
 
-num__btn.addEventListener("click", onSubmit);
+selection__btn.addEventListener("click", selectionSort);
