8c07c7e285deaf305845e632d1d0c10d9d309a1a
ddoniddoni <psdkei@naver.com>
2020-07-17 18:35:51 +0900

    Update selectionSort

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 const num__input = document.querySelector(".num__input");
 const selection__btn = document.querySelector(".selection__btn");
-let num__view = document.querySelector(".num__view");
 let numArray = [];
-
-// const onClick = () => {
-//   numArray = num__input.value.split(" ");
-//   for (let i = 0; i < numArray.length; i++) {
-//     console.log(numArray[i]);
-//   }
-// };
+const insert__num = document.querySelector(".insert__num");
+let num = document.createElement("span");
+let view__num = document.querySelector(".view__num");
 
 const selectionSort = () => {
   numArray = num__input.value.split(" ");
+  num.classList = "view__insert-num";
+  num.innerText = num__input.value;
+  insert__num.appendChild(num);
+
   let length = numArray.length;
   let minIndex, temp, i, j;
   for (i = 0; i < length - 1; i++) {
     minIndex = i;
-    console.log(`${i}번째 숫자는`, numArray[minIndex]);
+    const count = document.createElement("div");
+    count.classList = "num__count";
+    count.textContent = `${i + 1}번째 숫자는 ${numArray[minIndex]}`;
+    view__num.appendChild(count);
     for (j = i + 1; j < length; j++) {
       if (numArray[j] < numArray[minIndex]) {
         minIndex = j;
@@ -26,7 +28,14 @@ const selectionSort = () => {
     temp = numArray[minIndex];
     numArray[minIndex] = numArray[i];
     numArray[i] = temp;
+
+    const countArray = document.createElement("span");
+    countArray.classList = "num__span";
+    countArray.textContent = numArray;
+
+    count.appendChild(countArray);
     console.log(numArray);
+    num__input.value = "";
   }
 };
 
