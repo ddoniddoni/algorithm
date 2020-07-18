595e0e15c175c87eb4c8194cf34f127f568fa400
ddoniddoni <psdkei@naver.com>
2020-07-19 02:51:12 +0900

    Update BubbleSort

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const num__input = document.querySelector(".num__input");
 const selection__btn = document.querySelector(".selection__btn");
-let numArray = [];
+const bubble__btn = document.querySelector(".bubble__btn");
 const insert__num = document.querySelector(".insert__num");
 let num = document.createElement("span");
+let numArray = [];
 let view__num = document.querySelector(".view__num");
 
 const selectionSort = () => {
@@ -39,4 +40,35 @@ const selectionSort = () => {
   }
 };
 
+const bubbleSort = () => {
+  numArray = num__input.value.split(" ");
+  num.classList = "view__insert-num";
+  num.innerText = num__input.value;
+  insert__num.appendChild(num);
+
+  let length = numArray.length;
+  let i, j, temp;
+  for (i = 0; i < length - 1; i++) {
+    const count = document.createElement("div");
+    count.classList = "num__count";
+    count.textContent = `${i + 1}번째 숫자는 ${numArray[i]}`;
+    view__num.appendChild(count);
+    for (j = 0; j < length - 1 - i; j++) {
+      if (numArray[j] > numArray[j + 1]) {
+        temp = numArray[j];
+        numArray[j] = numArray[j + 1];
+        numArray[j + 1] = temp;
+      }
+    }
+    const countArray = document.createElement("span");
+    countArray.classList = "num__span";
+    countArray.textContent = numArray;
+
+    count.appendChild(countArray);
+    console.log(numArray);
+    num__input.value = "";
+  }
+};
+
 selection__btn.addEventListener("click", selectionSort);
+bubble__btn.addEventListener("click", bubbleSort);
