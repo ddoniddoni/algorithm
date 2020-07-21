da2537e5d270ecb0b7b514cde33f7ba507ae47db
ddoniddoni <psdkei@naver.com>
2020-07-21 18:37:26 +0900

    Update AnimationSort

---
+++ app.js
@@ -0,0 +1,32 @@
+const num__grp = document.querySelectorAll(".num__grp");
+const bubble__btn = document.querySelector(".bubble__btn");
+let num__array = [];
+
+const handleArray = () => {
+  for (let i = 0; i < num__grp.length; i++) {
+    num__array.push(Math.floor(num__grp[i].textContent));
+  }
+};
+
+handleArray();
+
+const bubbleSort = () => {
+  let length = num__array.length;
+  let i, j, temp;
+  for (i = 0; i < length - 1; i++) {
+    if (num__array[i] === Math.floor(num__grp[i].textContent)) {
+      num__grp[i].style.color = "red";
+    }
+    console.log(num__array[i], Math.floor(num__grp[i].textContent));
+    for (j = 0; j < length - 1 - i; j++) {
+      if (num__array[j] > num__array[j + 1]) {
+        temp = num__array[j];
+        num__array[j] = num__array[j + 1];
+        num__array[j + 1] = temp;
+      }
+    }
+  }
+  console.log(num__array);
+};
+
+bubble__btn.addEventListener("click", bubbleSort);
