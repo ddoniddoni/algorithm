fd0ffb53326d8c5ffa0d244c8282908e08c6fb1d
ddoniddoni <psdkei@naver.com>
2020-07-22 17:47:18 +0900

    Update Result

--- app.js
+++ app.js
@@ -14,10 +14,10 @@ const bubbleSort = () => {
   let length = num__array.length;
   let i, j, temp;
   for (i = 0; i < length - 1; i++) {
-    if (num__array[i] === Math.floor(num__grp[i].textContent)) {
-      num__grp[i].style.color = "red";
-    }
-    console.log(num__array[i], Math.floor(num__grp[i].textContent));
+    // if (num__array[i] === Math.floor(num__grp[i].textContent)) {
+    //   num__grp[i].style.color = "red";
+    // }
+    // console.log(num__array[i], Math.floor(num__grp[i].textContent));
     for (j = 0; j < length - 1 - i; j++) {
       if (num__array[j] > num__array[j + 1]) {
         temp = num__array[j];
@@ -27,6 +27,10 @@ const bubbleSort = () => {
     }
   }
   console.log(num__array);
+
+  for (let i = 0; i < length; i++) {
+    num__grp[i].textContent = `${num__array[i]}`;
+  }
 };
 
 bubble__btn.addEventListener("click", bubbleSort);
